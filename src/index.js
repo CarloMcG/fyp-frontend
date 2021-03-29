b3610480f9dc6612563969b2da566822a38cb613
Carlo <carlomcginley@gmail.com>
2021-03-29 18:10:50 +0100

    Revert "added support for AWS Log in"
    This reverts commit eb80c51442310bf48d92506e9d489d7f6769ae2c.

--- src/index.js
+++ src/index.js
@@ -1,17 +1,14 @@
-import React from "react";
-import ReactDOM from "react-dom";
-import "./index.css";
-import App from "./App";
-import reportWebVitals from "./reportWebVitals";
-import Amplify from "aws-amplify";
-import config from "./aws-exports";
-Amplify.configure(config);
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import App from './App';
+import reportWebVitals from './reportWebVitals';
 
 ReactDOM.render(
   <React.StrictMode>
     <App />
   </React.StrictMode>,
-  document.getElementById("root")
+  document.getElementById('root')
 );
 
 // If you want to start measuring performance in your app, pass a function
