3cdeb70a147d230adadfd337ca235321be320f78
Carlo <carlomcginley@gmail.com>
2021-03-30 18:52:47 +0100

    mplemented frontend

---
+++ src/AddToPlan.js
@@ -0,0 +1,92 @@
+import React from "react";
+import Container from "react-bootstrap/Container";
+import { Row } from "react-bootstrap";
+import { Col } from "react-bootstrap";
+import Form from "react-bootstrap/Form";
+
+function AddToPlan() {
+  return (
+    <Container>
+      <nav class="navbar navbar-expand-lg navbar-light bg-light">
+        <a class="navbar-brand" href="/CustomerDashboard">
+          TelecomsCorp
+        </a>
+        <button
+          class="navbar-toggler"
+          type="button"
+          data-toggle="collapse"
+          data-target="#navbarSupportedContent"
+          aria-controls="navbarSupportedContent"
+          aria-expanded="false"
+          aria-label="Toggle navigation"
+        >
+          <span class="navbar-toggler-icon"></span>
+        </button>
+
+        <div class="collapse navbar-collapse" id="navbarSupportedContent">
+          <ul class="navbar-nav mr-auto">
+            <li class="nav-item">
+              <a class="nav-link" href="/AdminDashboard">
+                Dashboard
+              </a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link active" href="/Plans">
+                Plans<span class="sr-only">(current)</span>
+              </a>
+            </li>
+          </ul>
+        </div>
+        <div class="btn-group float-right">
+          <a
+            href="/"
+            class="btn btn-primary btn-lg active"
+            role="button"
+            aria-pressed="true"
+          >
+            logout
+          </a>
+        </div>
+      </nav>
+      <Col class="row justify-content-center">
+        <div class="jumbotron vertical-center row justify-content-center">
+          <Form class="form-inline text-center">
+            <div class="form-inline form-group">
+              <label class="fLabel">User</label>
+              <input
+                type="text"
+                id="uID"
+                placeholder="User ID"
+                class="form-control form-fixer m-1"
+              ></input>
+            </div>
+            <div class="form-inline form-group">
+              <label class="fLabel">Plan</label>
+              <input
+                type="text"
+                id="uID"
+                placeholder="Plan ID"
+                class="form-control form-fixer m-1"
+              ></input>
+            </div>
+
+            <div class="row justify-content-center">
+              <div class="btn-group float-right">
+                <a
+                  href="/plans"
+                  class="btn btn-primary btn-lg active"
+                  role="button"
+                  aria-pressed="true"
+                >
+                  Save Changes
+                </a>
+              </div>
+            </div>
+          </Form>
+        </div>
+      </Col>
+    </Container>
+  );
+}
+
+export default AddToPlan;
---
+++ src/PrePayDashboard.js
@@ -0,0 +1,131 @@
+import React from "react";
+import Button from "react-bootstrap/Button";
+import Form from "react-bootstrap/Form";
+import Container from "react-bootstrap/Container";
+import { Row } from "react-bootstrap";
+import { Col } from "react-bootstrap";
+import { Line, Circle } from "rc-progress";
+
+function PrePayDashboard() {
+  return (
+    <Container fluid>
+      <nav class="navbar navbar-expand-lg navbar-light bg-light">
+        <a class="navbar-brand" href="/CustomerDashboard">
+          TelecomsCorp
+        </a>
+        <button
+          class="navbar-toggler"
+          type="button"
+          data-toggle="collapse"
+          data-target="#navbarSupportedContent"
+          aria-controls="navbarSupportedContent"
+          aria-expanded="false"
+          aria-label="Toggle navigation"
+        >
+          <span class="navbar-toggler-icon"></span>
+        </button>
+
+        <div class="collapse navbar-collapse" id="navbarSupportedContent">
+          <ul class="navbar-nav mr-auto">
+            <li class="nav-item active">
+              <a class="nav-link" href="/PrePayDashboard">
+                Dashboard<span class="sr-only">(current)</span>
+              </a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="/Transactions">
+                Transactions
+              </a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="/TopUp">
+                TopUp
+              </a>
+            </li>
+          </ul>
+        </div>
+        <div class="btn-group float-right">
+          <a
+            href="/"
+            class="btn btn-primary btn-lg active"
+            role="button"
+            aria-pressed="true"
+          >
+            logout
+          </a>
+        </div>
+      </nav>
+      <h1 class="float-left">Hello, $User</h1>
+
+      <div class="row justify-content-center">
+        <div class="jumbotron vertical-center row justify-content-center">
+          <div class="card-group ">
+            <div class="card">
+              <h6 class="card-header">Current Balance:</h6>
+              <div class="card-body">
+                <h1>€100.00</h1>
+              </div>
+            </div>
+            <div class="card ">
+              <h6 class="card-header">Mobile Minutes:</h6>
+              <div class="card-body">
+                <Circle
+                  percent="10"
+                  strokeWidth="6"
+                  trailwidth="8"
+                  strokeColor="#03DAC5"
+                  class="linear"
+                />
+                <h6 class="card-text row justify-content-center">120/360</h6>
+              </div>
+            </div>
+
+            <div class="card">
+              <h6 class="card-header">Landline Minutes:</h6>
+              <div class="card-body">
+                <Circle
+                  percent="10"
+                  strokeWidth="6"
+                  trailwidth="8"
+                  strokeColor="#03DAC5"
+                  class="linear"
+                />
+                <h6 class="card-text row justify-content-center">120/360</h6>
+              </div>
+            </div>
+
+            <div class="card">
+              <h6 class="card-header">International Minutes:</h6>
+              <div class="card-body">
+                <Circle
+                  percent="10"
+                  strokeWidth="6"
+                  trailwidth="8"
+                  strokeColor="#03DAC5"
+                  class="linear"
+                />
+                <h6 class="card-text row justify-content-center">120/360</h6>
+              </div>
+            </div>
+
+            <div class="card">
+              <h6 class="card-header">Premium Rate Minutes:</h6>
+              <div class="card-body">
+                <Circle
+                  percent="10"
+                  strokeWidth="6"
+                  trailwidth="8"
+                  strokeColor="#03DAC5"
+                  class="linear"
+                />
+                <h6 class="card-text row justify-content-center">120/360</h6>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </Container>
+  );
+}
+
+export default PrePayDashboard;
