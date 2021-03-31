a3739d0f86ab794bbf7b15e59dc001ac1effd098
Carlo <carlomcginley@gmail.com>
2021-03-31 14:12:26 +0100

    aws amplify impl

--- src/aws-exports.js
+++ src/aws-exports.js
@@ -3,10 +3,10 @@
 
 const awsmobile = {
     "aws_project_region": "eu-west-1",
-    "aws_cognito_identity_pool_id": "eu-west-1:a5cdfb3e-718c-4944-b106-d1599812132c",
+    "aws_cognito_identity_pool_id": "eu-west-1:31d8a769-26ed-45c1-bcda-71ca7465833e",
     "aws_cognito_region": "eu-west-1",
-    "aws_user_pools_id": "eu-west-1_RpaTCUkpQ",
-    "aws_user_pools_web_client_id": "27lud4o6teeti75cm1s2trhib7",
+    "aws_user_pools_id": "eu-west-1_YD2OPi2Xt",
+    "aws_user_pools_web_client_id": "3lka1j5gk4ci9viif7b7sfb59s",
     "oauth": {}
 };
 
